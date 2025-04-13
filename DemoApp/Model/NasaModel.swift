9f6a0d39fb00017ebb3379d33b97b98a549167a7
Raul juliao <raulj@uninorte.edu.co>
2025-04-13 17:42:25 -0500

    Model attributes for response

--- DemoApp/Model/NasaModel.swift
+++ DemoApp/Model/NasaModel.swift
@@ -5,3 +5,32 @@
 //  Created by Raul Humberto Juliao Colina on 10/04/25.
 //
 
+struct NasaModel{
+    let id: Int
+    let roverName: String
+    let earthDate: String
+    let imgURL: String
+    let fullName: String
+    let landingDate: String
+    let launchDate: String
+    
+    var roversNameS: String {
+        return roverName.capitalized
+    }
+    var earthDateS: String {
+        return earthDate.split(separator: "-").reversed().joined(separator: "/")
+    }
+    var fullNameS: String {
+        return fullName.capitalized
+    }
+    var launchDateS: String {
+        return launchDate.split(separator: "-").reversed().joined(separator: "/")
+    }
+    var landingDateS: String {
+        return landingDate.split(separator: "-").reversed().joined(separator: "/")
+    }
+    var urlString: String {
+        return imgURL
+    }
+    
+}
